2d1771c5d082b94a87497539470419940fd8d3f1
xsmetankaj <smetanka321@gmail.com>
2021-10-17 18:14:21 +0200

    add firebase json and setting up @angular/fire

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
+  firebase: {
+    projectId: 'oauth-cf55f',
+    appId: '1:396064736451:web:bbfae098a3a8244a9dce76',
+    storageBucket: 'oauth-cf55f.appspot.com',
+    apiKey: 'AIzaSyDdavaq5GNLXjGPbuBctzOZrb1XGrYomIc',
+    authDomain: 'oauth-cf55f.firebaseapp.com',
+    messagingSenderId: '396064736451',
+    measurementId: 'G-DRLW03RLDN',
+  },
   production: true
 };
